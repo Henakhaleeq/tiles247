b3c10aee0ba2cb373fcb08a61243390aaa8e4957
henakhaleeq <hena.khaleeq@ki5.co.uk>
2023-09-26 14:27:52 +0500

    Tiles247homepage done till usb bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,22 +30,52 @@ module.exports = {
       "3xs": "10px",
       inherit: "inherit",
     },
-    screens: {
-      lg: {
-        max: "1200px",
-      },
-      md: {
-        max: "960px",
-      },
-      sm: {
-        max: "420px",
-      },
-      desktopL: {
-        raw: "screen and (max-width: 1300px)",
-      },
-    },
+    // screens: {
+    //   lg: {
+    //     max: "1200px",
+    //   },
+    //   md: {
+    //     max: "960px",
+    //   },
+    //   sm: {
+    //     max: "420px",
+    //   },
+    //   xl: {
+    //     raw: "screen and (max-width: 1300px)",
+    //   },
+    // },
   },
   corePlugins: {
     preflight: false,
   },
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '540px',
+          },
+          '@screen md': {
+            maxWidth: '752px',
+          },
+          '@screen lg': {
+            maxWidth: '976px',
+          },
+          '@screen xl': {
+            maxWidth: '1280px',
+          },
+        }
+      })
+    },
+    function ({ addVariant }) {
+      addVariant('mobile-only', "@media screen and (max-width: 767px)"); // instead of hard-coded 640px use sm breakpoint value from config. Or anything
+    },
+    function ({ addVariant }) {
+      addVariant('Tab-only', "@media screen and (min-width: 768px) and (max-width: 1023px)"); // instead of hard-coded 
+    },
+    function ({ addVariant }) {
+      addVariant('Lap-only', "@media screen and (min-width: 1024px)"); // instead of hard-coded 
+    },
+  ]
 };
